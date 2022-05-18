3b71e0856f9ce5cf092ec28a812ee8a5e86f0da7
Frank Parejo <francisco.parejo.lopez@gmail.com>
2022-05-18 17:50:51 +0200

    fix layout styles.

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,13 +2,16 @@ import React, {FC} from "react";
 import Image from 'next/image'
 
 import styles from './Layout.module.css';
+import Link from "next/link";
 
 
 const Layout: FC = ({ children }) => (
   <div className={styles.wrapper}>
     <header className={styles.header}>
       <div className={styles.imageWrapper}>
-        <Image src="/logo.png" alt="Kvartal" width="140" height="100" />
+        <Link href="/">
+          <Image src="/logo.png" alt="Kvartal" width="140" height="100" />
+        </Link>
       </div>
     </header>
 
