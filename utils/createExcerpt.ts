619dc08f2848a6512cc012d1f73fa922e5b87799
Marcos Collado <marcoscolladosegura@ied.edu>
2022-05-18 11:44:16 +0200

    add formatters

---
+++ utils/createExcerpt.ts
@@ -0,0 +1,6 @@
+const createExcerpt = (content: string, excerptLength: number) => {
+    const excerpt = content.split(' ').slice(0, excerptLength).join(' ');
+    return excerpt;
+}
+
+export default createExcerpt;
