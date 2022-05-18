b8b12b5efa157ae19ae63a4e83ed09be88c44ec9
Frank Parejo <francisco.parejo.lopez@gmail.com>
2022-05-18 17:58:21 +0200

    add logo cursor pointer.

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,15 +1,14 @@
 import React, {FC} from "react";
-import Image from 'next/image'
-
-import styles from './Layout.module.css';
+import Image from 'next/image';
 import Link from "next/link";
 
+import styles from './Layout.module.css';
 
 const Layout: FC = ({ children }) => (
   <div className={styles.wrapper}>
     <header className={styles.header}>
       <div className={styles.imageWrapper}>
-        <Link href="/">
+        <Link href="/" className={styles.logoWrapper}>
           <Image src="/logo.png" alt="Kvartal" width="140" height="100" />
         </Link>
       </div>
