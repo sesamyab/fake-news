3951a89993c0d5932a3c7ea1c935c7158306bf65
Frank Parejo <francisco.parejo.lopez@gmail.com>
2022-05-25 11:31:38 +0200

    fix: use API_URL as env.

--- constants.ts
+++ constants.ts
@@ -1,4 +1,2 @@
-export const API_URL = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}/api`
-    : 'http://localhost:3000/api';
+export const API_URL = process.env.API_URL;
 export const NEXT_PUBLIC_PRODUCT_URL_TEST = process.env.NEXT_PUBLIC_PRODUCT_URL_TEST;
