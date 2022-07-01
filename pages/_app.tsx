2f418cb0e4bc56a826786e7c26637fc43ed19561
Markus Ahlstrand <markus@ahlstrand.de>
2022-07-01 15:44:59 +0200

    fix: stuuuuupid me

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 // font awesome
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
+import React from 'react';
+import Script from 'next/script';
 
 import '../styles/fonts.css';
 import '../styles/globals.css';
@@ -9,7 +11,18 @@ import '../styles/globals.css';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    <React.Fragment>
+        <Component {...pageProps} />
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+        <noscript>
+            {/* eslint-disable @next/next/no-img-element */}
+            <img
+                src="https://queue.simpleanalyticscdn.com/noscript.gif"
+                alt=""
+                referrerPolicy="no-referrer-when-downgrade"
+            />
+        </noscript>
+    </React.Fragment>;
 }
 
 export default MyApp;
