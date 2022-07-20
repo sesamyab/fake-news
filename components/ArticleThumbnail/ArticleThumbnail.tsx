1f1c8e8a1022c2526159931b3900518575b4f2be
Marcos Collado <marcoscolladosegura@ied.edu>
2022-07-20 11:49:02 +0200

    format date

--- components/ArticleThumbnail/ArticleThumbnail.tsx
+++ components/ArticleThumbnail/ArticleThumbnail.tsx
@@ -8,39 +8,40 @@ interface Props {
     article: Article;
 }
 
-const ArticleThumbnail: FC<Props> = ({ article }) => (
-    <li>
-        <Link href={`/${article.slug}`}>
-            <a className={styles.wrapper}>
-                <div className={styles.imageWrapper}>
-                    <Image
-                        alt={article.title}
-                        src={article.image}
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="50% 50%"
-                    />
-                </div>
-                <div className={styles.text}>
-                    <p className={styles.description}>{article.price}</p>
-                    <h3 className={styles.title}>{article.title}</h3>
-                    <p className={styles.description}>
-                        {intlFormat(
-                            new Date(article.date),
-                            {
-                                weekday: 'long',
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric',
-                            },
-                            {
-                                locale: 'se-SE',
-                            }
-                        )}
-                    </p>
-                </div>
-            </a>
-        </Link>
-    </li>
-);
+const ArticleThumbnail: FC<Props> = ({ article }) => {
+    const formattedDate = intlFormat(
+        new Date(article.date),
+        {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        },
+        {
+            locale: 'se-SE',
+        }
+    ).toString();
+    return (
+        <li>
+            <Link href={`/${article.slug}`}>
+                <a className={styles.wrapper}>
+                    <div className={styles.imageWrapper}>
+                        <Image
+                            alt={article.title}
+                            src={article.image}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="50% 50%"
+                        />
+                    </div>
+                    <div className={styles.text}>
+                        <p className={styles.description}>{article.price}</p>
+                        <h3 className={styles.title}>{article.title}</h3>
+                        <p className={styles.date}>{formattedDate}</p>
+                    </div>
+                </a>
+            </Link>
+        </li>
+    );
+};
 export default ArticleThumbnail;
