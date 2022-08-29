0c9e9af7b59eb5708de7ef270db7a89eec744a25
Marcos Collado <marcoscolladosegura@ied.edu>
2022-08-29 16:29:46 +0200

    update correctly the loggedin states

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+<<<<<<< HEAD
+=======
+import { useRouter } from 'next/router';
+
+enum Variant {
+    Picture = 'picture',
+    Logout = 'logout',
+    SesamyText = 'sesamy-text',
+}
+>>>>>>> ff996bf... update correctly the loggedin states
 
 const LoginButton = () => {
+    const { pathname } = useRouter();
+    const styleLogged = {
+        '--logged-color': pathname !== '/' ? '#030303' : '#f3f3f3',
+    } as React.CSSProperties;
+    const loggoutStyle = {
+        // will apply when variant is merged
+        '--logout-color': pathname !== '/' ? '#030303' : '#f3f3f3',
+    } as React.CSSProperties;
     return (
+<<<<<<< HEAD
         <div>
             <sesamy-login></sesamy-login>
+=======
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <sesamy-login style={styleLogged} client-id="sesamy"></sesamy-login>
+            <sesamy-login
+                style={loggoutStyle}
+                client-id="sesamy"
+                variant={Variant.Logout}
+            ></sesamy-login>
+>>>>>>> ff996bf... update correctly the loggedin states
         </div>
     );
 };
