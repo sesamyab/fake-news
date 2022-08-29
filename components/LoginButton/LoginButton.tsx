1f177be27162bfe98d289074ef2c146cf1d4a7b9
Marcos Collado <marcoscolladosegura@ied.edu>
2022-08-29 16:27:04 +0200

    fix login

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
+
 import { useRouter } from 'next/router';
 
 enum Variant {
@@ -8,7 +7,6 @@ enum Variant {
     Logout = 'logout',
     SesamyText = 'sesamy-text',
 }
->>>>>>> ff996bf... update correctly the loggedin states
 
 const LoginButton = () => {
     const { pathname } = useRouter();
@@ -20,10 +18,6 @@ const LoginButton = () => {
         '--logout-color': pathname !== '/' ? '#030303' : '#f3f3f3',
     } as React.CSSProperties;
     return (
-<<<<<<< HEAD
-        <div>
-            <sesamy-login></sesamy-login>
-=======
         <div style={{ display: 'flex', alignItems: 'center' }}>
             <sesamy-login style={styleLogged} client-id="sesamy"></sesamy-login>
             <sesamy-login
@@ -31,7 +25,6 @@ const LoginButton = () => {
                 client-id="sesamy"
                 variant={Variant.Logout}
             ></sesamy-login>
->>>>>>> ff996bf... update correctly the loggedin states
         </div>
     );
 };
