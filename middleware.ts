53f38e41cb54de17860e54950d80d2ff6d9d578b
Daniel Gent <dan@sesamy.com>
2022-09-01 18:55:54 +0200

    chore: remove comments

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,7 @@ import type { NextRequest } from 'next/server';
 // Only rewrite files that don't have a file extension - https://github.com/vercel/examples/blob/main/edge-functions/i18n/pages/_middleware.ts#L9
 const PUBLIC_FILE = /\.(.*)$/;
 
-// This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
-    // TODO - can now use matchers, so don't need all this custom complexity
-
     // Bail out from non-page routes
     if (PUBLIC_FILE.test(req.nextUrl.pathname)) {
         return undefined;
@@ -20,7 +17,6 @@ export function middleware(req: NextRequest) {
         return undefined;
     }
 
-    // TODO - this is not secure! see how it's done on other libraries
     if (req.nextUrl.pathname.includes('403')) {
         return undefined;
     }
@@ -31,7 +27,6 @@ export function middleware(req: NextRequest) {
         const authValue = basicAuth.split(' ')[1];
         const [user, pwd] = atob(authValue).split(':');
 
-        // TODO - env vars
         if (user === 'admin' && pwd === 'password') {
             return undefined;
         }
@@ -43,14 +38,3 @@ export function middleware(req: NextRequest) {
 
     return badAuthRes;
 }
-
-/*
-
-TODO - learn this so can hopefully just match on real pages and not assets
-
-export const config = {
-  matcher: '/',
-}
-
-
-*/
